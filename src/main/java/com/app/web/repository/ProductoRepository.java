fb55d4d39e47122dfd275c6276af71e3618b2a04
User <User>
2022-12-13 11:29:17 -0500

    Commit Proyecto Web

--- src/main/java/com/app/web/repository/ProductoRepository.java
+++ src/main/java/com/app/web/repository/ProductoRepository.java
@@ -3,8 +3,8 @@ package com.app.web.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.app.web.entities.Producto;
 
+import com.app.web.entities.Producto;
 @Repository
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
 
